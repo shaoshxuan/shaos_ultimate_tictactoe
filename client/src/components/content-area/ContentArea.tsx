8f7f01b43cac794cb29a82c88928a1b3d5e39c69
Chua Shao Shxuan <shaoshxuan.chua@gmail.com>
2022-07-09 17:51:07 +0800

    Update ContentArea.tsx

--- client/src/components/content-area/ContentArea.tsx
+++ client/src/components/content-area/ContentArea.tsx
@@ -1,18 +1,11 @@
 import AppRoutes from "../../AppRoutes";
-import MainGrid from "./components/main_grid/MainGrid";
-import { ThemeContext } from "../../contexts/themeContext";
 import "./ContentArea.scss";
-import { useContext } from "react";
-import HomeMenu from "./components/home_menu/HomeMenu";
 
 const ContentArea = () => {
-    const {darkMode} = useContext(ThemeContext);
     
     return (
         <div className="ContentArea">
             <AppRoutes />
-            {/* <MainGrid /> */}
-            {/* <HomeMenu /> */}
         </div>
     )
 }
